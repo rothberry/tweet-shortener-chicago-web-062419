f75202411fad75f34e51881014daca3ada52deef
Phil Roth <phil.roth077@gmail.com>
2019-06-13 16:11:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -3,20 +3,20 @@ require 'pry'
 tweet_test1 = "Hello, this is for you!"
 tweet_test2 = "Be yourself and see you at the party, too"
 
-def dictionary
-  dictionary_hash{
-    "hello" => "hi",
-    "to" => "2",
-    "two" => "2",
-    "too" => "2",
-    "for" => "4",
-    "four" => "4",
-    "be" => "b",
-    "you" => "u",
-    "at" => "@",
-    "and" => "&"
-  }
-end
+# def dictionary
+#   dictionary_hash{
+#     "hello" => "hi",
+#     "to" => "2",
+#     "two" => "2",
+#     "too" => "2",
+#     "for" => "4",
+#     "four" => "4",
+#     "be" => "b",
+#     "you" => "u",
+#     "at" => "@",
+#     "and" => "&"
+#   }
+# end
 
 def word_substituter(tweet)
 binding.pry
@@ -24,3 +24,4 @@ binding.pry
 
 
 end
+s
