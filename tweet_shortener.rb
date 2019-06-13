5b9aa04b396a4ae6c3832f48e106b234f135b033
Phil Roth <phil.roth077@gmail.com>
2019-06-13 16:02:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+# "hello" becomes 'hi'
+# "to, two, too" become '2' 
+# "for, four" become '4'
+# 'be' becomes 'b'
+# 'you' becomes 'u'
+# "at" becomes "@" 
+# "and" becomes "&"
+def dictionary
+  dictionary_hash{
+    "hi" => "hello",
+    "2" => ["to", "two", "too"],
+    "4" => ["for", "four"],
+    "b" => "be",
+    "at" => "@",
+    "and" => "&"
+  }
+  
+end
