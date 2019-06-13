3d8a9653ce9b9796850976cf9e8c2fe033a59a5b
Phil Roth <phil.roth077@gmail.com>
2019-06-13 16:05:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,5 @@
 # Write your code here.
-# "hello" becomes 'hi'
-# "to, two, too" become '2'
-# "for, four" become '4'
-# 'be' becomes 'b'
-# 'you' becomes 'u'
-# "at" becomes "@"
-# "and" becomes "&"
 def dictionary
-
   dictionary_hash{
     "hello" => "hi",
     "to" => "2",
